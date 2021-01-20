b4069976c19728423851ce37a89f0181abc5e3c9
shane <hunghsuyong114shane@gmail.com>
2021-01-20 19:23:47 +0800

    PCA step by step update & dolphinsound.wav feature extraction

--- soundfile/wave_read.py
+++ soundfile/wave_read.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import soundfile as sf 
-data, fs = sf.read("forest.wav")
-print(type(data))
-print(len(data))
+data, fs = sf.read("10MG.wav")
+print("fs is ", fs)
+print("data type is", data.dtype)
+print("data shape is ", data.shape)
+print("data length is ", len(data))
 for i in data[:10]:
-	print(type(i))
-	print(i)
+    print(i)
+    print("")
