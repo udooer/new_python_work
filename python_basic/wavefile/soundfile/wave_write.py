e305225823e56ff8a7fc6f9d592e7ce92f51d91e
shane <hunghsuyong114shane@gmail.com>
2021-01-22 17:00:24 +0800

    rearrange the file system and add the final PCA report in the folder /PCA

---
+++ python_basic/wavefile/soundfile/wave_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import soundfile as sf 
+import numpy as np 
+data = np.array([[0,0]], dtype="float32")
+for i in range(1, 20):
+	data = np.concatenate((data, np.array([[i, -i]], dtype="float32")), axis=0)
+print(data.shape)
+print(data.dtype)
+print(data)
+sf.write("test.wav", data, 96000)
